7ac7af74b093335633acf5615028503d646548ab
Jonas Terp <jonas.terp@skola.botkyrka.se>
2020-02-17 12:38:49 +0100

    Handling match-data in dictionaries

--- get_data.py
+++ get_data.py
@@ -4,14 +4,16 @@ data = pd.read_csv(r'football_data.csv') # All data
 columns = pd.DataFrame(data, columns= ['HomeTeam','AwayTeam', 'FTHG','FTAG','HTHG','HTAG','HS','AS','HST','AST','HY','AY','HR','AR'])
 print(columns)
 
-'''
-for x in range(380):
-    for y in columns.loc[x,:]:
-        print(y)
-'''
+lst_of_matches = {}
+i = 0 # Möjligt att iterera alla matcher med loop
+temp_dict = {}
 
-row = columns.loc[0,:]
-print(row[2])
+for j in range(14):
+    temp_dict[columns.columns[j]] = columns.loc[i,:][j]
+lst_of_matches[i] = temp_dict
+
+print(lst_of_matches)
+print(lst_of_matches[0]["HomeTeam"]) # Möjligt att kalla på värden inom matcher
+
+row = columns.loc[0,:] # Skapar en kolumn för en enskild match
 print(row)
-for y in (row):
-    print(y)
