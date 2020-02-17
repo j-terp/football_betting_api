d3947258b14ba5ec1b708070a81c0b5976e8eb6c
Elliot Stjernqvist <elliot.stjernqvist@gmail.com>
2020-02-17 10:03:03 +0100

    Working on list comprehension

--- get_data.py
+++ get_data.py
@@ -3,5 +3,15 @@ import pandas as pd
 data = pd.read_csv(r'football_data.csv') # All data
 columns = pd.DataFrame(data, columns= ['HomeTeam','AwayTeam', 'FTHG','FTAG','HTHG','HTAG','HS','AS','HST','AST','HY','AY','HR','AR'])
 print(columns)
-row0 = columns.loc[0,:]
-print(row0[2])
+
+'''
+for x in range(380):
+    for y in columns.loc[x,:]:
+        print(y)
+'''
+
+row = columns.loc[0,:]
+print(row[2])
+print(row)
+for y in (row):
+    print(y)
