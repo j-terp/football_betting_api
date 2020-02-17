a0cb95e6bf80c24507810c8c9b77639984f7f37f
Elliot Stjernqvist <elliot.stjernqvist@gmail.com>
2020-02-17 09:43:28 +0100

    Functional row selection

--- get_data.py
+++ get_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 data = pd.read_csv(r'football_data.csv') # All data
-columns = pd.DataFrame(data, columns= ['HomeTeam','AwayTeam','FTR','HTHG','HTAG','HS','AS','HST','AST','HY','AY','HR','AR'])
-print(columns.loc[0,:])
+columns = pd.DataFrame(data, columns= ['HomeTeam','AwayTeam', 'FTHG','FTAG','HTHG','HTAG','HS','AS','HST','AST','HY','AY','HR','AR'])
+print(columns)
+row0 = columns.loc[0,:]
+print(row0[2])
