c68bed8c5e975ed148ac006ab8d1438d10b692b1
Jonas Terp <jonas.terp@skola.botkyrka.se>
2020-02-18 16:04:22 +0100

    Clarified variables and lines of code

--- get_data.py
+++ get_data.py
@@ -1,19 +1,20 @@
 import pandas as pd
 
 data = pd.read_csv(r'football_data.csv') # All data
-columns = pd.DataFrame(data, columns= ['HomeTeam','AwayTeam', 'FTHG','FTAG','HTHG','HTAG','HS','AS','HST','AST','HY','AY','HR','AR'])
-print(columns)
+relevant_data = pd.DataFrame(data, columns= ['HomeTeam','AwayTeam', 'FTHG','FTAG','HTHG','HTAG','HS','AS','HST','AST','HY','AY','HR','AR'])
+print(relevant_data)
 
-lst_of_matches = {}
+list_of_matches = {}
 i = 0 # Möjligt att iterera alla matcher med loop
 temp_dict = {}
 
 for j in range(14):
-    temp_dict[columns.columns[j]] = columns.loc[i,:][j]
-lst_of_matches[i] = temp_dict
+    temp_dict[relevant_data.columns[j]] = relevant_data.loc[i,:][j]
+list_of_matches[i] = temp_dict
 
-print(lst_of_matches)
-print(lst_of_matches[0]["HomeTeam"]) # Möjligt att kalla på värden inom matcher
+print(list_of_matches)
+print(list_of_matches[0]["HomeTeam"]) # Möjligt att kalla på specifika värden inom matcher
+# lst_ofmatches[index för matchens rad][target-data]
 
-row = columns.loc[0,:] # Skapar en kolumn för en enskild match
+row = relevant_data.loc[0,:] # Skapar en kolumn för en enskild match
 print(row)
