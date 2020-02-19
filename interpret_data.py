1c1c2b9df1a3db104dd6c346c2b08db091267fea
Elliot Stjernqvist <elliot.stjernqvist@gmail.com>
2020-02-19 13:51:32 +0100

    First attempt at comparing teams using half time goals and shots on target

--- interpret_data.py
+++ interpret_data.py
@@ -8,6 +8,48 @@ match_list = dataset_to_dictionary(relevant_data) # Konverterar datan till dicti
 
 row = single_match(0)
 
+"""
 print(relevant_data)
 print(match_list)
 print(row)
+print(match_list[2])
+print(match_list[2]["AY"])
+"""
+def HTG_compare(match):
+    hometeam = match_list[match]['HTHG']
+    awayteam = match_list[match]['HTAG']
+
+    if hometeam > awayteam:
+        return 1
+    elif hometeam == awayteam:
+        return 0
+    else:
+        return -1
+
+def ST_compare(match):
+    hometeam = match_list[match]['HST']
+    awayteam = match_list[match]['AST']
+
+    if hometeam > awayteam:
+        return 0.5
+    elif hometeam == awayteam:
+        return 0
+    else:
+        return -0.5
+
+def winning_team(team_score):
+    print(team_score)
+    if team_score > 0:
+        print("Home team wins")
+    elif team_score < 0:
+        print("Away team wins")
+    else:
+        print("Draw")
+
+for y in match_list:
+    team_score = HTG_compare(y)
+    team_score += ST_compare(y)
+    winning_team(team_score)
+
+
+
