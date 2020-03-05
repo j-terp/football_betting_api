603ea48c277d9cf1aedc438fa23b9eeafdb443b1
Jonas Terp <jonas.terp@skola.botkyrka.se>
2020-03-05 10:44:07 +0100

    csv clean function

--- test.py
+++ test.py
@@ -1,23 +1,6 @@
 from get_data import pd, dataset_to_dictionary
-fetched_dataframe = pd.read_csv(r'dataframe.csv')
-
 dictionary_keys = []
-for col in fetched_dataframe.columns:
+for col in pd.read_csv(r'dataframe.csv').columns:
     dictionary_keys.append(col)
-print(dictionary_keys)
-
-fetched_dictionary = pd.DataFrame(fetched_dataframe).to_dict('index')
-print(fetched_dictionary)
-
-values = [["Volvo V70", 69420],["Saab", 39900]]
-addition_point = len(fetched_dictionary)
-for x in values:
-    temp_dict = {}
-    for key in dictionary_keys:
-        temp_dict[key] = x[dictionary_keys.index(key)]
-    fetched_dictionary[addition_point + values.index(x)] = temp_dict
-    print(fetched_dictionary)
-
-
-export_dictionary = pd.DataFrame.from_dict(fetched_dictionary, orient='index')
-export_csv = export_dictionary.to_csv(r'dataframe.csv', index = None, header=True) #Don't forget to add '.csv' at the end of the path
+#export_dataframe = pd.DataFrame.from_dict(fetched_dictionary, orient='index')
+export_csv = pd.DataFrame(columns=dictionary_keys).to_csv(r'dataframe.csv', index = None, header=True) #Don't forget to add '.csv' at the end of the path
